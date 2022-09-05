044aea4db16a2826d7156d5f23120fa724ffe4e6
Mustafa Haider <42119893+mustafahaider508@users.noreply.github.com>
2022-09-05 12:10:11 -0700

    updated

--- src/Components/BeforeFooter.js
+++ src/Components/BeforeFooter.js
@@ -1,7 +1,7 @@
 import { Divider, Grid } from '@mui/material'
 import { Container } from '@mui/system'
 import React from 'react'
-import {faBullhorn, faClockFour, faTruckFast } from '@fortawesome/free-solid-svg-icons';
+import { faClockFour, faTruckFast } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./BeforeFooter.css";
 
