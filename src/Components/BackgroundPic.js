0c4c403910be69a81416b1a72f29d03f3051b63b
Mustafa Haider <42119893+mustafahaider508@users.noreply.github.com>
2022-09-24 08:56:15 -0700

    updates

--- src/Components/BackgroundPic.js
+++ src/Components/BackgroundPic.js
@@ -1,14 +1,30 @@
+import { Grid ,Hidden} from '@mui/material';
 import React from 'react';
 import { Link } from "react-router-dom";
+import Mob_banner from '../Mob_banner';
+import './BackgroundPic.css';
 
 
 function BackgroundPic() {
   return (
     <>
+    <Hidden smDown>
     <Link to="/home">
-    <div style={{backgroundImage:"url('/Site_Banner.png')",backgroundSize:"cover",height:"131.5vh",position:"relative",backgroundPosition:"50%",marginTop:"-230px"}}>
+      <Grid container>
+        <Grid item xs={12} md={12} lg={12}>
+        <div style={{backgroundImage:"url('/Site_Banner.png')",backgroundSize:"cover",height:"131.5vh",position:"relative",backgroundPosition:"50%",marginTop:"-230px"}}>
 </div>
+        </Grid>
+      </Grid>
+   
      </Link>
+     </Hidden>
+
+     <Hidden smUp>
+     <Mob_banner />
+     </Hidden>
+
+    
       
     </>
   )
---
+++ src/Mob_banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Grid ,Hidden} from '@mui/material';
+import { Link } from "react-router-dom";
+
+
+function Mob_banner() {
+  return (
+    <>
+   <Link to="/home">
+      <Grid container>
+        <Grid item xs={12} md={12} lg={12}>
+        <div style={{backgroundImage:"url('/Site_Banner_Mob.png')",backgroundSize:"cover",height:"100vh",position:"relative",backgroundPosition:"50%",marginTop:"0px"}}>
+</div>
+        </Grid>
+      </Grid>
+   
+     </Link>
+    </>
+  )
+}
+
+export default Mob_banner
